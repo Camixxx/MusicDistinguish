b4df285706cff33b34fa1476db3c61688955f60d
Camixxx <hatikoy@163.com>
2017-03-10 14:50:05 +0800

    初始化

---
+++ static/muse.js
@@ -0,0 +1,18 @@
+
+var audio;
+var framePlayer;
+window.onload = function(){
+      initAudio();
+
+}
+
+function testAI(isAI) {
+
+}
+function initAudio() {
+    // audio = document.getElementById('player');
+    // audio.src = "static/music/gen/302.mp3"
+    // audio.play();
+    framePlayer =document.getElementById('frame_player');
+    framePlayer.src="/player";
+}
